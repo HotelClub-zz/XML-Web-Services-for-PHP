c5df955a066d3ad107fecdb7d15f77c081f35ef4
HotelClub <robert.love@hotelclub.com>
2010-12-10 12:37:31 +1100

    Updated licence text

--- library/Content.php
+++ library/Content.php
@@ -5,9 +5,8 @@
  * LICENSE
  *
  * This source file is subject to the new BSD license that is bundled with this
- * package in the file LICENSE.txt. It is also available through the
- * world-wide-web at this URL:
- * http://www.opensource.org/licenses/bsd-license.php
+ * package in the file LICENSE. It is also available through the world-wide-web
+ * at this URL: http://www.opensource.org/licenses/bsd-license.php
  * If you did not receive a copy of the license and are unable to obtain it
  * through the world-wide-web, please send an email to license@hotelclub.com so
  * we can send you a copy immediately.
